137cd3cf99a018e416cbd371e607f3b9aeb89ee0
artemowkin <aloskan@sdvor.com>
2024-03-30 20:47:55 +0700

    Removed environment

--- src/server.go
+++ src/server.go
@@ -25,8 +25,7 @@ func initSentry() {
 	log.Printf("Initing sentry with dsn: %s", settings.Settings.APP_SENTRY_DSN)
 	if settings.Settings.APP_SENTRY_DSN != "" {
 		err := sentry.Init(sentry.ClientOptions{
-			Dsn:         settings.Settings.APP_SENTRY_DSN,
-			Environment: "stage",
+			Dsn: settings.Settings.APP_SENTRY_DSN,
 		})
 		if err != nil {
 			log.Printf("Error sentry init: %s", err)
